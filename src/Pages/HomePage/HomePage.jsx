3b983a92a7b69bc16227e55433a6207726cd6835
unknown <industrialdesignoscar@gmail.com>
2024-11-27 22:32:08 -0500

    ADD: firebase

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './HomePage.scss';
-import SimpleSlider from '../../Components/SimpleSlider/SimpleSlider';
+import slide1 from '../../img/slide1-home.jpg'
 
 const HomePage = () => {
   return (
     <div className='homepage-block'>
-
+      <div className="homepage__section">
+        <img className='homepage__image' src={slide1} alt="slide1" />
+      </div>
 
     </div>
   )
---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWy_xTJqj3AC2Z7W9lYHWZKZmcbE4XNLY",
+  authDomain: "domotics-chris-rosas.firebaseapp.com",
+  projectId: "domotics-chris-rosas",
+  storageBucket: "domotics-chris-rosas.firebasestorage.app",
+  messagingSenderId: "486375839803",
+  appId: "1:486375839803:web:691076ead3e0b98765241e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
